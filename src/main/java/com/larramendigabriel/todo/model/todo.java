c2c1aaaf488edf36294b67c6878a8c166956f522
Larramendi <larramendiga@gmail.com>
2023-04-26 19:33:47 -0300

    Creating Todo JPA Entity

---
+++ src/main/java/com/larramendigabriel/todo/model/todo.java
@@ -0,0 +1,28 @@
+package com.larramendigabriel.todo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "todos")
+public class todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false) //JPA creates, automatically, a column with attribute's name. If you want a different name, you have to use 'name' attribute in @Column.
+    private String description;
+
+    private Boolean complete;
+}
