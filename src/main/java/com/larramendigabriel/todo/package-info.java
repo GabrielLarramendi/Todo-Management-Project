016d3cf65456eecd8c0946aa5feada5f322d9521
Larramendi <larramendiga@gmail.com>
2023-04-26 18:58:49 -0300

    First commit

---
+++ src/main/java/com/larramendigabriel/todo/package-info.java
@@ -0,0 +1,1 @@
+package com.larramendigabriel.todo;
---
+++ src/test/java/com/larramendigabriel/todo/TodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larramendigabriel.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
