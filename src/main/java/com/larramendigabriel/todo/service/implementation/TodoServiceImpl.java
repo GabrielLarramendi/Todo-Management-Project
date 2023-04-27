14929e324658af573c45550b5ef5a26e9e45055b
Larramendi <larramendiga@gmail.com>
2023-04-26 23:21:56 -0300

    Using ModelMappar library to convert JPA entity to DTO and vice versa

--- src/main/java/com/larramendigabriel/todo/service/implementation/TodoServiceImpl.java
+++ src/main/java/com/larramendigabriel/todo/service/implementation/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import com.larramendigabriel.todo.model.Todo;
 import com.larramendigabriel.todo.repository.TodoRepository;
 import com.larramendigabriel.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,25 +13,18 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDTO addTodo(TodoDTO todoDTO) {
 
         //First convert DTO to JPA Entity to save in database
-        Todo todoJPA = new Todo();
-        todoJPA.setTitle(todoDTO.getTitle());
-        todoJPA.setDescription(todoDTO.getDescription());
-        todoJPA.setCompleted(todoDTO.getCompleted());
-
+        Todo todoJPA = modelMapper.map(todoDTO, Todo.class);
         //Then save de Object in database
         Todo savedTodoJPA = todoRepository.save(todoJPA);
 
         //Now convertTodo JPA Entity to DTO in order to return a DTO object
-        TodoDTO savedTodoDTO = new TodoDTO();
-        savedTodoDTO.setId(savedTodoJPA.getId());
-        savedTodoDTO.setTitle(savedTodoJPA.getTitle());
-        savedTodoDTO.setDescription(savedTodoJPA.getDescription());
-        savedTodoDTO.setCompleted(savedTodoJPA.getCompleted());
+        TodoDTO savedTodoDTO = modelMapper.map(savedTodoJPA, TodoDTO.class);
 
         return savedTodoDTO;
     }
